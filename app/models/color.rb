cd0a15ea612bc3a5eb24d59309ab21251b629c79
Emily Lordahl <contact@emilylordahl.com>
2015-10-22 13:46:28 -0400

    password digest fix

--- app/models/color.rb
+++ app/models/color.rb
@@ -2,4 +2,14 @@ class Color < ActiveRecord::Base
 
 	has_and_belongs_to_many :users
 
+	def self.get_colors
+		response = HTTParty.get('https://www.kimonolabs.com/api/dpq9elac?apikey=nSRHFLygw6rJNhaX3W2LKsLCwPPOudme')
+		p response['results']['collection'][0]['color']['href']
+
+		# response.each do |i|
+		# 	category = ['results']['collection'][i]['color']['href']
+		# end
+		# p category
+	end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,17 @@ class User < ActiveRecord::Base
 	has_secure_password
 	has_and_belongs_to_many :colors, dependent: :destroy
 
-	validates :name, presence: true
-	validates :email, presence: true
-	validates :email, uniqueness: true
-	validates :email, :email => {:strict_mode => true}
-	validates :password, presence: true, length: { minimum: 8 }, 
-		unless: :skip_password_validation # Allows updating user information without updating password
-	validates :password_confirmation, presence: true, 
-		unless: :skip_password_confirmation_validation # Allows updating user information without updating password
-	validates :password_confirmation, confirmation: true
+	# validates :name, presence: true
+	# validates :email, presence: true
+	# validates :email, uniqueness: true
+	# validates :email, :email => {:strict_mode => true}
+	# validates :password, presence: true, length: { minimum: 8 }
+	# 	# unless: :skip_password_validation # Allows updating user information without updating password
+	# validates :password_confirmation, presence: true 
+	# 	# unless: :skip_password_confirmation_validation # Allows updating user information without updating password
+	# validates :password_confirmation, confirmation: true
 
 	# Allows updating user information without updating password
-	attr_accessor :skip_password_validation, :skip_password_confirmation_validation
+	# attr_accessor :skip_password_validation, :skip_password_confirmation_validation
 
 end
