dd0243bfe1f7957c1df19eb5845eef8519bca3bf
Emily Lordahl <contact@emilylordahl.com>
2015-10-23 14:54:28 -0400

    adds sessions functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root to: 'application#index'
-  resources :colors
   resources :users
+  resources :colors
+
+  # Login Form
+  get 'sessions/new' => 'sessions#new'
+  # Login the user
+  post 'sessions' => 'sessions#create'
+  # Logout the user
+  delete 'sessions' => 'sessions#destroy'
 end
