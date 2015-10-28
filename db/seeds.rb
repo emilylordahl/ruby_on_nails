075a02c63123cc65726435164e397d336a1f54c1
Emily Lordahl <contact@emilylordahl.com>
2015-10-27 23:58:35 -0400

    seed file for color data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Seed Color API
+
+Color.destroy_all
+
+# Data Scraped from Essie's Website
+response = HTTParty.get('https://www.kimonolabs.com/api/dpq9elac?apikey=nSRHFLygw6rJNhaX3W2LKsLCwPPOudme')
+i = 0
+
+while i < response["results"]["collection"].length do
+
+  Color.create({
+    name: response["results"]["collection"][i]["color"]["text"].titleize,
+    hex_value: response["results"]["collection"][i]["color"]["style"].split(" ")[1],
+    category: response["results"]["collection"][i]["color"]["href"].split("/")[4].capitalize
+  })
+
+  i += 1
+end
